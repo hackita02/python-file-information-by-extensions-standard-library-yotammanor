d7b2c37ed007793b026318e3d11606cf0c396de7
Yotam <yotammanor@gmail.com>
2015-10-26 18:57:55 +0200

    working calendar

--- server.py
+++ server.py
@@ -1,14 +1,35 @@
-from bottle import route, run, template
+from bottle import route, run, template, redirect, view
 import os
 import pathlib
+import calendar
+import datetime
 
 PORT = int(os.environ.get("PORT", 5000))
 BASE_PATH = pathlib.Path(__file__).parent
 
-@route('/hello')
-@route('/hello/<name>')
-def index(name):
+
+@route('/hello/')
+@route('/hello/<name>/')
+def index(name='world'):
     return template('hello_world', name=name)
+
+
+@route('<path:re:.+[^/]$>')
+def add_slash(path):
+    return redirect(path + "/")
+
+
+@route('/')
+@route('/<year>/')
+@route('/<year>/<month>/')
+@view('calendar')
+def month_view(year=None, month=None):
+    year_param = year or datetime.datetime.now().year
+    month_param = month or datetime.datetime.now().month
+    prev = '/{}/{}'.format(year_param, int(month_param) - 1)
+    next_month = '/{}/{}'.format(year_param, int(month_param) + 1)
+    return dict(cal=calendar.HTMLCalendar().formatmonth(int(year_param), int(month_param)), prev=prev,
+                next=next_month)
 
 
 run(host='0.0.0.0', port=PORT)
---
+++ views/calendar.tpl
@@ -0,0 +1,7 @@
+<body>
+  {{! cal}}
+
+  <hl>
+  <p><a href={{prev}}>Previous</a></p>
+  <p><a href={{next}}>Next</a></p>
+</body>
