e2ee7c06b44385998b58fcd300c0ada3589a1747
Yotam <yotammanor@gmail.com>
2015-10-26 17:23:28 +0200

    server with template

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 from bottle import route, run, template
 
+
+@route('/hello')
 @route('/hello/<name>')
 def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+    return template('hello_world', name=name)
+
 
 run(host='localhost', port=8080)
